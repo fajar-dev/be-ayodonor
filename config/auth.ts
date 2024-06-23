a77609bfbacac613991a8fef59cd1ce7ea2383ae
fajar-dev <fajarrivaldi2015@gmail.com>
2024-06-23 14:23:48 +0700

    fix: fixing bug user

--- config/auth.ts
+++ config/auth.ts
@@ -1,28 +1,28 @@
-// import { defineConfig } from '@adonisjs/auth'
-// import { tokensGuard, tokensUserProvider } from '@adonisjs/auth/access_tokens'
-// import type { InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
+import { defineConfig } from '@adonisjs/auth'
+import { tokensGuard, tokensUserProvider } from '@adonisjs/auth/access_tokens'
+import type { InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
 
-// const authConfig = defineConfig({
-//   default: 'api',
-//   guards: {
-//     api: tokensGuard({
-//       provider: tokensUserProvider({
-//         tokens: 'accessTokens',
-//         model: () => import('#models/user')
-//       }),
-//     }),
-//   },
-// })
+const authConfig = defineConfig({
+  default: 'api',
+  guards: {
+    api: tokensGuard({
+      provider: tokensUserProvider({
+        tokens: 'accessTokens',
+        model: () => import('#models/user')
+      }),
+    }),
+  },
+})
 
-// export default authConfig
+export default authConfig
 
-// /**
-//  * Inferring types from the configured auth
-//  * guards.
-//  */
-// declare module '@adonisjs/auth/types' {
-//   interface Authenticators extends InferAuthenticators<typeof authConfig> {}
-// }
-// declare module '@adonisjs/core/types' {
-//   interface EventsList extends InferAuthEvents<Authenticators> {}
-// }
+/**
+ * Inferring types from the configured auth
+ * guards.
+ */
+declare module '@adonisjs/auth/types' {
+  interface Authenticators extends InferAuthenticators<typeof authConfig> {}
+}
+declare module '@adonisjs/core/types' {
+  interface EventsList extends InferAuthEvents<Authenticators> {}
+}
